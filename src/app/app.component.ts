b4b276fa3608d6354fc61e694b827145fcc769c9
jannik_riegel <jannik.riegel@gmail.com>
2025-05-14 23:37:44 +0200

    menu page created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'yemen';
+  constructor(private router: Router) {}
+
+  goToMenu() {
+    console.log('goToMenu called');
+    this.router.navigate(['/menu']);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'menu', component: MenuComponent }];
